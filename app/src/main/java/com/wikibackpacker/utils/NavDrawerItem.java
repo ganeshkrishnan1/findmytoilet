f8ca5bf496a82c5e29457154e56de970f44f7090
savan <panarasavan@gmail.com>
2016-05-07 11:07:28 +0530

    Menu Style Change Final

---
+++ app/src/main/java/com/wikibackpacker/utils/NavDrawerItem.java
@@ -0,0 +1,37 @@
+package com.wikibackpacker.utils;
+
+public class NavDrawerItem {
+
+    private String title;
+    private int icon;
+
+    public NavDrawerItem() {
+    }
+
+    public NavDrawerItem(String title, int icon) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public NavDrawerItem(String title, int icon, boolean isCounterVisible) {
+        this.title = title;
+        this.icon = icon;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+}
